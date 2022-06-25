99c24097b621e1e020953771df5810ed76ae1b04
Folzo1 <chrisd1231@gmail.com>
2022-06-25 10:15:53 +0800

    Initial Commit

---
+++ Twodos/TwodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwodosApp.swift
+//  Twodos
+//
+//  Created by T Krobot on 25/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TwodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
